96fe2cee0cbcc6ce680fa10abdce2a88449d5ffe
Matthew Sinex <matthew.sinex@healthcatalyst.com>
2022-04-08 20:50:07 -0400

    markdown support hallelujah

--- projects/soup-kit-web/src/app/app.module.ts
+++ projects/soup-kit-web/src/app/app.module.ts
@@ -1,5 +1,7 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MarkdownModule } from 'ngx-markdown';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    MarkdownModule.forRoot({ loader: HttpClient }),
   ],
   providers: [],
   bootstrap: [AppComponent]
